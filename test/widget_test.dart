d017720408b737fc5c887f3ff6b6fda229149544
bgheorghiu <bogdangheorghiu7@gmail.com>
2020-12-15 13:31:10 +0200

    newchange

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,42 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_apps/main.dart';
+import 'package:lib/src/h4_movies.dart' as app;
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets(
+    'Counter increments smoke test',
+        (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(app.MovieListApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Verify that our counter starts at 0.
+      expect(
+        find.text('0'),
+        findsOneWidget,
+      );
+      expect(
+        find.text('1'),
+        findsNothing,
+      );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(
+        find.byIcon(
+          Icons.add,
+        ),
+      );
+      await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+      // Verify that our counter has incremented.
+      expect(
+        find.text('0'),
+        findsNothing,
+      );
+      expect(
+        find.text('1'),
+        findsOneWidget,
+      );
+    },
+  );
 }
