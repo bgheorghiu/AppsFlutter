9d67a68dab90a343b5158b94df104f3f7a8ce0f5
bgheorghiu <bogdan.gheorghiu@harman.com>
2020-12-22 01:34:21 +0200

    addmovie

---
+++ lib/src/movie_redux/models/movie.dart
@@ -0,0 +1,25 @@
+library movie;
+
+import 'package:built_value/serializer.dart';
+import 'package:built_value/built_value.dart';
+import 'package:flutter_app/src/models/serializers.dart';
+
+part 'movie.g.dart';
+
+abstract class Movie implements Built<Movie, MovieBuilder>{
+
+  factory Movie([void Function(MovieBuilder) updates]) = _$Movie;
+
+  factory Movie.fromJson(dynamic json){
+    return serializers.deserializeWith(serializer, json);
+  }
+
+  Movie._();
+
+  String get title;
+
+  @BuiltValueField(wireName: 'medium_cover_image')
+  String get mediumCoverImage;
+
+  static Serializer<Movie> get serializer => _$movieSerializer;
+}
